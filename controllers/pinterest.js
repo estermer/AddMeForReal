8e15ac3c7befc63fc7afe0264bfd6c21a67de968
estermer <eric.stermer@gmail.com>
2016-10-04 21:59:38 -0400

    created auth routes for pinterest using passport-pinterest

--- controllers/pinterest.js
+++ controllers/pinterest.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var PinterestStrategy = require('passport-pinterest').Strategy;
+var User = require('../models/user.js');
+var addAccessCodeToUser = require('../public/js/add-access-code-to-user.js');
 
-router.get('/', function(req,res){
-  res.send('I WORK!');
+var PINTEREST_APP_ID = "4860479268236313660";
+var PINTEREST_APP_SECRET = "8cc6bcb94a7a5075de5348d19160962d6d3d7fc04ca64603b14c115ed1e0f22e";
+
+passport.use(new PinterestStrategy({
+        clientID: PINTEREST_APP_ID,
+        clientSecret: PINTEREST_APP_SECRET,
+        scope: ['read_public', 'read_relationships'],
+        callbackURL: "https://localhost:3000/pinterest/auth/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // User.findOrCreate({ pinterestId: profile.id }, function (err, user) {
+        //     return done(err, user);
+        // });
+    }
+));
+
+router.get('/auth', passport.authenticate('pinterest'));
+
+router.get('/auth/callback', function(req, res){
+    var username = req.user.username
+    User.findOne({username: username}, function(err, user){
+      if(err)console.log(err);
+
+      //check if a code has already been aquired and added into user model
+      //if not then add it to the user model
+      addAccessCodeToUser(user, user.socialPlatforms, "pinteerest", req.query.code);
+
+      res.redirect('/' + req.user.username);
+    });
 });
+
+module.exports = router;
+
 
 module.exports = router;
--- controllers/youtube.js
+++ controllers/youtube.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var YoutubeV3Strategy = require('passport-youtube-v3').Strategy;
+var User = require('../models/user.js');
+var addAccessCodeToUser = require('../public/js/add-access-code-to-user.js');
 
-router.get('/', function(req,res){
-  res.send('I WORK!');
+var YOUTUBE_CLIENT_ID = "1033837139381-254vohtgjnaeefqs99hfptlvdrqj3ir6.apps.googleusercontent.com";
+var YOUTUBE_CLIENT_SECRET = "ii1a0hdJFZyqgyXZkMx9GNiX";
+
+passport.use(new YoutubeV3Strategy({
+    clientID: YOUTUBE_CLIENT_ID,
+    clientSecret: YOUTUBE_CLIENT_SECRET,
+    callbackURL: "http://localhost:1992/youtube/auth/callback",
+    scope: ['https://www.googleapis.com/auth/youtube']
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate({ userId: profile.id }, function (err, user) {
+    //   return done(err, user);
+    // });
+  }
+));
+
+router.get('/auth', passport.authenticate('youtube'));
+
+router.get('/auth/callback', function(req, res){
+    var username = req.user.username
+    User.findOne({username: username}, function(err, user){
+      if(err)console.log(err);
+
+      //check if a code has already been aquired and added into user model
+      //if not then add it to the user model
+      addAccessCodeToUser(user, user.socialPlatforms, "youtube", req.query.code);
+
+      res.redirect('/' + req.user.username);
+    });
 });
 
 module.exports = router;
