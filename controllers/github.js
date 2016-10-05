74feea79411646044a43bace95a6fe00a8889004
estermer <eric.stermer@gmail.com>
2016-10-04 21:20:04 -0400

    Added npm module passport-youtube-v3 for oauth

--- controllers/github.js
+++ controllers/github.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var passport = require('passport');
 var GitHubStrategy = require('passport-github2').Strategy;
 var User = require('../models/user.js');
+var addAccessCodeToUser = require('../public/js/add-access-code-to-user.js');
+
 
 var GITHUB_CLIENT_ID = "89221658f77bf282f490";
 var GITHUB_CLIENT_SECRET = "11f6d9c96c884834e4d3c4cfc0b8cd5c32231c52";
@@ -21,8 +23,17 @@ passport.use(new GitHubStrategy({
 
 router.get('/auth', passport.authenticate('github'));
 
-router.get('auth/callback', function(req, res){
+router.get('/auth/callback', function(req, res){
+  var username = req.user.username;
+  User.findOne({username: username}, function(err, user){
+    if(err)console.log(err);
 
+    //check if a code has already been aquired and added into user model
+    //if not then add it to the user model
+    addAccessCodeToUser(user, user.socialPlatforms, "github", req.query.code);
+
+    res.redirect('/' + req.user.username);
+  });
 });
 
 module.exports = router;
