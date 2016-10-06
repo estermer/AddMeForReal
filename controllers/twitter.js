37f7e94f267272a96a134346083f8573e5fb56da
estermer <eric.stermer@gmail.com>
2016-10-06 01:37:26 -0400

    update, working moving twitter oauth to app-only oauth using oauth2

--- controllers/twitter.js
+++ controllers/twitter.js
@@ -1,17 +1,20 @@
+var TWITTER_CONSUMER_KEY = "nDmyK0cdIv5PPhgjfVvaQhMBZ";
+var TWITTER_CONSUMER_SECRET = "QilHBSnd22V1ggFDltKlRanNL5oHTHtY1ZNHjEuDtkFaEufnz8";
+var REDIRECT_URL = "http://localhost:1992/twitter/auth/callback";
+
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var request = require('request');
 var TwitterStrategy = require('passport-twitter').Strategy;
 var User = require('../models/user.js');
 var addAccessCodeToUser = require('../public/js/add-access-code-to-user.js');
 
-var TWITTER_CONSUMER_KEY = "nDmyK0cdIv5PPhgjfVvaQhMBZ";
-var TWITTER_CONSUMER_SECRET = "QilHBSnd22V1ggFDltKlRanNL5oHTHtY1ZNHjEuDtkFaEufnz8";
 
 passport.use(new TwitterStrategy({
     consumerKey: TWITTER_CONSUMER_KEY,
     consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://localhost:1992/twitter/auth/callback"
+    callbackURL: REDIRECT_URL
   },
   function(token, tokenSecret, profile, cb) {
     // asynchronous verification, for effect...
@@ -25,16 +28,21 @@ passport.use(new TwitterStrategy({
 router.get('/auth', passport.authenticate('twitter'));
 
 router.get('/auth/callback', function(req, res){
-    var username = req.user.username
-    User.findOne({username: username}, function(err, user){
-      if(err)console.log(err);
-      console.log(req.query);
-      //check if a code has already been aquired and added into user model
-      //if not then add it to the user model
-      addAccessCodeToUser(user, user.socialPlatforms, "twitter", req.query.oauth_token);
+    // var accessCode = req.query.oauth_token;//8QwsFgAAAAAAxUB7AAABV5gNS7Y
+    // var verifier = req.query.oauth_verifier;
+    // var username = req.user.username;
+    console.log(req.query);
 
-      res.redirect('/' + req.user.username);
-    });
+
+    // User.findOne({username: username}, function(err, user){
+    //   if(err)console.log(err);
+    //   console.log(req.query);
+    //   //check if a code has already been aquired and added into user model
+    //   //if not then add it to the user model
+    //   addAccessCodeToUser(user, user.socialPlatforms, "twitter", req.query.oauth_token);
+    //
+    //   res.redirect('/' + req.user.username);
+    // });
 });
 
 module.exports = router;
